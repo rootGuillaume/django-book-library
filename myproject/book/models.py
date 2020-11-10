af1c03226144b399db193bb0f9236f5838bfc46f
rootGuillaume <guillaumegasmi@protonmail.com>
2020-11-10 23:34:43 +0100

    Update #2 -- Added book app

--- myproject/book/models.py
+++ myproject/book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    max_page = models.IntegerField(max_length=4)
+    current_page = models.IntegerField(max_length=4)
